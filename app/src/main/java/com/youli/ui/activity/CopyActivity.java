085c7162fd3c9c41e6ff9ef2d8edc982337a4276
liubei0619 <137034552+liubei0619@users.noreply.github.com>
2023-07-03 13:44:57 +0800

    first commit

---
+++ app/src/main/java/com/youli/ui/activity/CopyActivity.java
@@ -0,0 +1,28 @@
+package com.youli.ui.activity;
+
+import com.youli.R;
+import com.youli.app.AppActivity;
+
+/**
+ *    author : Android 轮子哥
+ *    github : https://github.com/getActivity/AndroidProject
+ *    time   : 2018/10/18
+ *    desc   : 可进行拷贝的副本
+ */
+public final class CopyActivity extends AppActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.copy_activity;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
